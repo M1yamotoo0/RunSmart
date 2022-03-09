ca27aeb399a3a4d2d224fb89024bf8b81c06fff4
Nikolay <nkd-da-k1ng@bk.ru>
2022-03-09 14:47:43 +0300

    media

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,13 @@
 $('.carousel__list').slick({
     prevArrow: '<button type="button" class="slick-prev"></button>',
-    nextArrow: '<button type="button" class="slick-next"></button>'
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            arrows: false,
+            dots: true
+          }
+        }
+      ]
 });
